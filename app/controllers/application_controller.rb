fb06010a4bee32ea8deb3da0b45b6c79240981c8
Ryan <junheng102@gmail.com>
2018-03-08 17:35:43 +0800

    generate user model and migration files

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,21 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+  helper_method :current_user
+
+  def logged_in?
+    !current_user.nil?
+  end
+  helper_method :logged_in?
+
+  def sign_in(user)
+    session[:user_id] = user.id
+  end
+
+  def cities
+    render json: CS.cities(params[:state], :my).to_json
+  end
 end
